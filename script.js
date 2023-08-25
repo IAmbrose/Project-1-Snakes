43676cd87227b42791ffbbbaaa3c50f0eae58647
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-08-25 11:40:29 +0800

    start the project

---
+++ script.js
@@ -0,0 +1,37 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+// snake position?
+// food position?
+ 
+/*----- cached elements  -----*/
+const grid = document.getElementById("grid");
+
+
+
+
+
+
+
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+
+
+
+function snakeHead() {};
+
+function snakeBody() {};
+
+function createFood() {};
+
+function drawFood() {};
+
+function eatFood() {};
+
+function direction() {};
