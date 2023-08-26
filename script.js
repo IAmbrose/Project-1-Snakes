4e2d09c06575677de8983c69da3301c6cdee6d46
ignatiusaa <ignatius.a.ambrose@gmail.com>
2023-08-26 16:58:12 +0800

    form the snake body

--- script.js
+++ script.js
@@ -6,15 +6,23 @@ const grid = document.getElementById("grid");
 /*----- state variables -----*/
 let snakePos = 0
 let foodPos = 0
-let keys = {
-    ArrowUp: true,
-    ArrowLeft: true,
-    ArrowRight: true,
-    ArrowDown: true
-};
+
 
 /*----- cached elements  -----*/
-
+class Queue {
+    constructor(size) {
+        this.size = size;
+        this.list =[];
+        this.push = function push(i) {
+            this.list.push(i)
+            if (this.list.length > this.size) {
+                this.list.shift();
+            }
+        }
+    }
+}
+const currentSnakeBody = new Queue(2);
+const tail = new Queue(2);
 
 
 
@@ -26,7 +34,7 @@ document.addEventListener("keydown", moveSnake);
 
 
 function createBoard() {
-    for (i = 0; i < 25; i++) {
+    for (i = 0; i < 100; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
         grid.append(box);
@@ -34,20 +42,21 @@ function createBoard() {
     }
 };
 
+// Function for the main snake position generation
 function snake() {
-    let arr = Array.from({length:25});
-    let index = Math.floor(Math.random() * 25);
-    for (index = Math.floor(Math.random() * 25); index === foodPos; index = Math.floor(Math.random() * 25)) {
-        index = Math.floor(Math.random() * 25);
+    let arr = Array.from({length:100});
+    let index = Math.floor(Math.random() * 100);
+    for (index = Math.floor(Math.random() * 100); index === foodPos; index = Math.floor(Math.random() * 100)) {
+        index = Math.floor(Math.random() * 100);
     }
     let snake = document.getElementById(index);
     snake.classList.add("snake");
     snakePos = index;
 };
 
-
+// Below are functions for the food
 function createFood() {
-    let randomIndex = Math.floor(Math.random() * 25);
+    let randomIndex = Math.floor(Math.random() * 100);
     let food = document.getElementById(randomIndex);
     food.classList.add("food");
     foodPos = randomIndex;
@@ -56,26 +65,50 @@ function createFood() {
 function eatFood() {
     if (snakePos === foodPos) {
         document.getElementById(foodPos).classList.remove("food");
+        currentSnakeBody.size += 1
         createFood();
     }
 };
 
+// function for the movement of the snake
 function moveSnake(e) {
     document.getElementById(snakePos).classList.remove("snake");
-    if (keys[e.key]) {
-        if (e.key === "ArrowUp") {
-            snakePos -= 5;
-        } else if (e.key === "ArrowDown") {
-            snakePos += 5;
-        } else if (e.key === "ArrowLeft") {
+    switch (e.key) {
+        case "ArrowUp":
+            snakePos -= 10;
+            break
+        case "ArrowDown":
+            snakePos += 10;
+            break
+        case "ArrowLeft":
             snakePos -= 1;
-        } else if (e.key === "ArrowRight") {
+            break
+        case "ArrowRight":
             snakePos += 1;
+            break
         } 
+        eatFood();
+        growSnake();
+        document.getElementById(snakePos).classList.add("snake");
     };
-    document.getElementById(snakePos).classList.add("snake");
-    eatFood();
+
+// Below are functions for managing the growing of the snake
+function growSnake() {
+    body();
+    clearOld();
+    currentSnakeBody.list.map((i) => document.getElementById(i).classList.add("snake"));
 };
+
+function body() {
+    currentSnakeBody.push(snakePos);
+    tail.push(currentSnakeBody.list[0]);
+};
+
+function clearOld() {
+    tail.list.map(i => document.getElementById(i).classList.remove("snake"));
+}
+
+
 
 
 
